f06c00aa68256568cd14d0bb3d234b7da9a13abe
tuanama13 <tuanama7@gmail.com>
2020-10-07 16:15:01 +0700

    Create Login

---
+++ src/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+      <div class="card login">
+          <div class="card-body">
+              <h2 class="card-title text-center">Login</h2>
+              <form @submit.prevent="login" class="text-center">
+                  <div class="form-group">
+                      <input type="text" v-model="name" name="name" class="form-control" placeholder="Please enter name...">
+                      <p v-if="errorText" class="text-danger">{{errorText}}</p>
+                  </div>
+              </form>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'login',
+    data(){
+        return {
+            name : "",
+            errorText: null,
+        }
+    },
+    methods: {
+        login(){
+            if(this.name){
+                this.$router.push({name: 'Chat', params: {name: this.name}})
+            }else{
+                // this.
+            }
+        },
+    },
+}
+</script>
+
+<style>
+
+</style>
